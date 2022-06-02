4727629bbe32a9155a1b9c5b350ef5692fb441eb
dreamanlan <dreaman_163@163.com>
2022-06-03 00:34:31 +0800

    ios&osx compile

--- build/make_osx_dylib.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p build_osx && cd build_osx
-cmake -DBUILD_AS_OSX_DYLIB=1 -GXcode ../../
-cd ..
-cmake --build build_osx --config Release
-cp -rf build_osx/Release/libboost_context.dylib Plugins/
--- build/make_osx_lib.sh
+++ build/make_osx_lib.sh
@@ -2,4 +2,4 @@ mkdir -p build_osx && cd build_osx
 cmake -GXcode ../../
 cd ..
 cmake --build build_osx --config Release
-cp -rf build_osx/Release/boost_context.bundle Plugins/
+cp -rf build_osx/Release/libboost_context.a Plugins/
