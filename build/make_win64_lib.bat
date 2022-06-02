1dfe0c8782de949f127ccd9adb1ca117ee2ea73e
dreamanlan <dreamanlan@tencent.com>
2022-06-02 11:22:32 +0800

    添加编译批处理脚本与依赖头文件

---
+++ build/make_win64_lib.bat
@@ -0,0 +1,11 @@
+mkdir build64 & pushd build64
+cmake -G "Visual Studio 17 2022" -A x64 ..\..
+popd
+cmake --build build64 --config Release --target boost_context ZERO_CHECK
+
+md Plugins\x86_64
+copy /Y build64\Release\boost_context.lib Plugins\x86_64\boost_context.lib
+
+rem rmdir /s /q .\build64
+
+pause
